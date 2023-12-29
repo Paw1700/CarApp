2651cffd9c70828c4ad1d8cec80842ee24ad6a0b
paw1700 <uryckip@gmail.com>
2023-12-29 20:44:32 +0100

    Adopted Angular App Bootstrap

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,8 +6,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: ``,
+  styles: `` 
 })
 export class AppComponent {
   
--- src/app/config.ts
+++ src/app/config.ts
@@ -1,7 +1,6 @@
 import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { routes } from './routing';
 import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
